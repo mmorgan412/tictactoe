d2242dd3a3d9d8f48b442d793bb5d36861fa64a6
mmorgan412 <melanie.morgan@libertymutual.com>
2017-09-26 20:43:00 -0400

    Add click events for cells
    Ability to add an X or an O background image when selecting the grid

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -12,3 +12,33 @@ $(() => {
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
+
+$('#row1col1').on('click', function () {
+  console.log('clicked 1st cell')
+  $('#row1col1').css('background-image', 'url(lib/images/o.png)')
+})
+$('#row1col2').on('click', function () {
+  $('#row1col2').css('background-image', 'url(lib/images/letter_x.png)')
+  console.log('clicked 2nd cell')
+})
+$('#row1col3').on('click', function () {
+  console.log('clicked 3rd cell')
+})
+$('#row2col1').on('click', function () {
+  console.log('clicked 1st cell')
+})
+$('#row2col2').on('click', function () {
+  console.log('clicked 2nd cell')
+})
+$('#row2col3').on('click', function () {
+  console.log('clicked 3rd cell')
+})
+$('#row3col1').on('click', function () {
+  console.log('clicked 1st cell')
+})
+$('#row3col2').on('click', function () {
+  console.log('clicked 2nd cell')
+})
+$('#row3col3').on('click', function () {
+  console.log('clicked 3rd cell')
+})
